32642998789e7e1cfb0e433d1b0b7899ef7e7be3
Erik <Erik@DESKTOP-FFKLVQ5>
2021-07-17 13:09:26 +0500

    Добавьте файлы проекта.

---
+++ lesson2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение переменной b: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("Введите значение переменной a: ");
+            int a = int.Parse(Console.ReadLine());
+            if (b > a)
+                Console.WriteLine("Ошибка! Переменная b > a");
+            int i, sum = 0;
+            for (i = b; a >= i; i++)
+                sum += i;
+            Console.WriteLine("Сумма чисел равна: " + sum);
+            Console.ReadLine();
+
+        }
+    }
+}
